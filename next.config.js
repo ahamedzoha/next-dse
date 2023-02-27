adf6a87dd84b7afbd21605e3cab64935c869c51a
ahamedzoha <ahamed.zoha@gmail.com>
2023-02-27 18:06:21 +0600

    added chakra ui, added SEO component and done basic cleanup

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: ['azazahamed.com', 'dse.azazahamed.com', 'localhost'],
+  },
+
+  // SVGR
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
