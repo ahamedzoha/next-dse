88eaafc9152ec18b392b60d97c9f14d4b3c9ab75
ahamedzoha <ahamed.zoha@gmail.com>
2023-03-01 13:19:35 +0600

    fixed: next auth by using firebase-admin, svcacckey

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import './globals.css'
 import Seo from '@/components/seo'
 import Navbar from '@/components/navbar'
+import SessionWrapper from '@/providers/session-provider'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
       <Seo templateTitle='Home' pathname='/' />
       <head />
-      <body>
-        <Navbar />
-        {children}
-      </body>
+      <SessionWrapper>
+        <body>
+          <Navbar />
+          {children}
+        </body>
+      </SessionWrapper>
     </html>
   )
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,20 +2,27 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { FirestoreAdapter } from '@next-auth/firebase-adapter'
 import { firestore } from '@/lib/firebase'
+import { cert } from 'firebase-admin/app'
 // import * as firestoreFunctions from 'firebase/firestore'
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 
-const web_client_id = process.env.WEB_CLIENT_ID || ''
-const web_client_secret = process.env.WEB_CLIENT_SECRET || ''
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || ''
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || ''
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: web_client_id,
-      clientSecret: web_client_secret,
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
-  adapter: FirestoreAdapter(firestore.app),
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    }),
+  }),
 })
