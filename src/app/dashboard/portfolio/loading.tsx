e2d428bb2e3964231a04baf7b7f645fde9433e1f
ahamedzoha <ahamed.zoha@gmail.com>
2023-03-04 13:27:51 +0600

    updated layout ui

--- src/app/dashboard/portfolio/loading.tsx
+++ src/app/dashboard/portfolio/loading.tsx
@@ -1,7 +1,71 @@
+'use client'
 const PortfolioLoading = () => {
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 opacity-60'>
+      <div className='w-full bg-slate-900 drop-shadow-md rounded-lg'>
+        <div className='animate-pulse w-full h-48 bg-slate-700'></div>
+        <div className='p-3 space-y-4'>
+          <div className='animate-pulse w-2/3 h-6 bg-slate-700'></div>
+          <div className='flex space-x-4'>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+          </div>
+          <div className='space-y-2'>
+            <div className='animate-pulse w-3/4 h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-full h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-2/3 h-3 bg-slate-500'></div>
+          </div>
+        </div>
+      </div>
+      <div className='w-full bg-slate-900 drop-shadow-md rounded-lg'>
+        <div className='animate-pulse w-full h-48 bg-slate-700'></div>
+        <div className='p-3 space-y-4'>
+          <div className='animate-pulse w-2/3 h-6 bg-slate-700'></div>
+          <div className='flex space-x-4'>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+          </div>
+          <div className='space-y-2'>
+            <div className='animate-pulse w-3/4 h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-full h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-2/3 h-3 bg-slate-500'></div>
+          </div>
+        </div>
+      </div>
+      <div className='w-full bg-slate-900 drop-shadow-md rounded-lg'>
+        <div className='animate-pulse w-full h-48 bg-slate-700'></div>
+        <div className='p-3 space-y-4'>
+          <div className='animate-pulse w-2/3 h-6 bg-slate-700'></div>
+          <div className='flex space-x-4'>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+          </div>
+          <div className='space-y-2'>
+            <div className='animate-pulse w-3/4 h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-full h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-2/3 h-3 bg-slate-500'></div>
+          </div>
+        </div>
+      </div>
+      <div className='w-full bg-slate-900 drop-shadow-md rounded-lg'>
+        <div className='animate-pulse w-full h-48 bg-slate-700'></div>
+        <div className='p-3 space-y-4'>
+          <div className='animate-pulse w-2/3 h-6 bg-slate-700'></div>
+          <div className='flex space-x-4'>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+            <div className='animate-pulse w-1/3 h-3 bg-slate-400'></div>
+          </div>
+          <div className='space-y-2'>
+            <div className='animate-pulse w-3/4 h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-full h-3 bg-slate-500'></div>
+            <div className='animate-pulse w-2/3 h-3 bg-slate-500'></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,8 +1,34 @@
-const Home = () => {
+import Image from 'next/image'
+import { DefaultUser, getServerSession } from 'next-auth'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+
+const getUser = async () => {
+  const session = await getServerSession(authOptions)
+
+  return session
+}
+
+const Home = async () => {
+  const user = await getUser()
+
+  console.log(user)
+
   return (
-    <div>
-      Enter
-      <h1>Home</h1>
+    <div className='w-full flex flex-1'>
+      <div className='flex space-x-6'>
+        <Image
+          src={user?.user?.image ? user.user.image : '/images/user.png'}
+          alt='hero image'
+          width={100}
+          height={100}
+          className='w-24 h-24 rounded-full'
+        />
+
+        <div className='flex flex-col'>
+          <h2 className='text-3xl font-bold'>{user?.user?.name}</h2>
+          <p className='text-gray-300'>{user?.user?.email}</p>
+        </div>
+      </div>
     </div>
   )
 }
